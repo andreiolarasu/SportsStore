1995c803a0bde09963f8281b60fc62e48627b05c
andrei.olarasu <andrei.olarasu@PC1568>
2016-12-22 15:34:37 +0200

    Add project files.

---
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI {
+
+    public class RouteConfig {
+
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(null,
+                "",
+                new {
+                    controller = "Product", action = "List",
+                    category = (string)null, page = 1
+                }
+            );
+
+            routes.MapRoute(null,
+                "Admin",
+                new { controller = "Admin", action = "Index" }
+            );
+
+            routes.MapRoute(null,
+                "Admin/Product/{productId}",
+                new { controller = "Admin", action = "Edit" }
+            );
+
+            routes.MapRoute(null,
+                "Admin/Product/{productId}",
+                new { controller = "Admin", action = "Delete" }
+            );
+
+            routes.MapRoute(null,
+                "Admin/Product/{productId}",
+                new { controller = "Admin", action = "Create" }
+            );
+
+            routes.MapRoute(null,
+                "Page{page}",
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
+        }
+    }
+}
